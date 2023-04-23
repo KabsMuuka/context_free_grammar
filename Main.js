c5dc0f2a0b3bdebf8ee05c105be3bceb234ce33b
Gabriel18711 <122276140+Gabriel18711@users.noreply.github.com>
2023-04-24 00:56:23 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,16 +1,17 @@
 // Grammar rules
 let rules = {
-    "start": ["#Greeting# #Name#!", "#Task#", "#Team#"],
+    "start": ["#Greeting# #Name#!", "#Task#", "#Team#", "#JavaScript#"],
     "Task": ["We have been given an assignment on Context Free Grammar! And this is a sample code.",
              "Our task is to implement a context-free grammar in Javascript.",
              "The assignment is about generating random sentences using context-free grammar.",
              "We need to create a program that generates sentences based on a set of rules."],
     "Greeting": ["Hello", "Hi", "Hey", "Greetings", "Safe", "How are you doing", "Yo", "What's up"],
-    "Name": ["Kabs Muuka", "Bob", "Edward Jr", "Gilbert"],
+    "Name": ["Kabs Muuka", "Bob", "Edward Jr", "Gabriel"],
     "Team": ["#Name# is in group 7, and we are a team of 5 members.",
              "Our team consists of #Name#, #Name#, #Name#, #Name#, and #Name#.",
              "In our team, we have #Name#, #Name#, #Name#, #Name#, and #Name#.",
              "We are a team of 5 members, including #Name#, #Name#, #Name#, #Name#, and #Name#."]
+    "Javascript" : ["#Name# loves using Javascript.",]
   };
   
   let grammar;
